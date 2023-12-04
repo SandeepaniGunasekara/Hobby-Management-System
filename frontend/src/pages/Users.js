19fada7193055d1cafd50d96c28a293fb23cfe14
ChethiyaNuwan <gunasekaramgms.20@uom.lk>
2023-12-04 10:20:18 +0530

    created user list page

--- frontend/src/pages/Users.js
+++ frontend/src/pages/Users.js
@@ -1,8 +1,47 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+
+const columns = [
+  { id: "id", label: "ID" },
+  { id: "name", label: "Name" },
+  { id: "email", label: "Email" },
+  { id: "address", label: "Address" },
+];
+
 export default function Users() {
-         
-    return (
-        <div>
-            <h1>Users</h1>
-        </div>
-    );
+  const users = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com",
+      address: "1234 Main St",
+    },
+  ];
+
+  return (
+    <Table id='userTable'>
+      <TableHead>
+        <TableRow>
+          {columns.map((column) => (
+            <TableCell key={column.id}>{column.label}</TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {users.map((row) => (
+          <TableRow key={row.id}>
+            {columns.map((column) => (
+              <TableCell>{row[column.id]}</TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
 }
