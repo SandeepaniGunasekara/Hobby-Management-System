1b424b9f00fbc2703de0f7717f030e71ddbad475
ChethiyaNuwan <gunasekaramgms.20@uom.lk>
2023-12-04 10:04:23 +0530

    configured protected route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import Login from './pages/Login';
+import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Login from "./pages/Login";
+import Users from "./pages/Users";
+import ProtectedRoute from "./utils/ProtectedRoute";
 
 function App() {
   const routes = createBrowserRouter([
-    {path: '/login', element: <Login/>},
+    { path: "/login", element: <Login /> },
+    { path: "/users", element: <ProtectedRoute><Users/></ProtectedRoute> },
   ]);
 
-  return (
-    <RouterProvider router={routes}/>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
---
+++ frontend/src/utils/ProtectedRoute.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import { Route, Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (user) {
+      setLoggedIn(true);
+    }
+  }, []);
+
+  return loggedIn ? children : <Navigate to="/login" />;
+}
