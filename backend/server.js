237079f83da5462479ed2682ec65329820610641
ChethiyaNuwan <gunasekaramgms.20@uom.lk>
2023-12-04 11:26:02 +0530

    configured backend with database

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const sql = require('mssql2');
-
-const dbConfig = {
-    "user": "root",
-    "password": "",
-    "server": "localhost",
-    "database": "hobby"
-};
+const sql = require("mysql2");
+const cors = require("cors");
 
 const port = 5000;
 
-app.get('/login', (req, res) => res.send('Hello World!'));
+const dbConfig = {
+  user: "root",
+  password: "",
+  server: "localhost",
+  database: "hobby",
+};
+
+const db = new sql.createConnection(dbConfig);
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/login", (req, res) => res.send("Hello World!"));
 
 app.listen(port, () => console.log(`listening on port ${port}!`));
