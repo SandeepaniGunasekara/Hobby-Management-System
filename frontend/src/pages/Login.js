37a326aa25370ff449d63843e0c436eeb25a1d61
ChethiyaNuwan <gunasekaramgms.20@uom.lk>
2023-12-04 11:16:44 +0530

    added login api call

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,14 +1,45 @@
 import React, { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
 
 export default function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [message, setMessage] = useState('');
+    const navigate = useNavigate();
   
     const handleSubmit = (event) => {
       event.preventDefault();
+      const emailRegex = /\S+@\S+\.\S+/;
+      if (!emailRegex.test(email)) {
+        setMessage("Invalid email");
+        return;
+      }
+
+      const credentials = {email: email, password: password};
+      
+      fetch('login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
+      })
+      .then(response => 
+        response.json()
+      )
+      .then(data => {
+        if (data.user) {
+          localStorage.setItem('user', data.user);
+          navigate('/users');
+        } else {
+          setMessage(data.message);
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
     };
   
     return (
@@ -22,6 +53,8 @@ export default function Login() {
             variant="outlined"
             value={email}
             onChange={(event) => setEmail(event.target.value)}
+            type='email'
+            required
           />
           <TextField
             id="password"
@@ -30,6 +63,7 @@ export default function Login() {
             type="password"
             value={password}
             onChange={(event) => setPassword(event.target.value)}
+            required
           />
           <Button type="submit" variant="contained" color="primary">
             Login
