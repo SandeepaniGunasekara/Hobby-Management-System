69b999d7d895fe44261ff57302e40f69b9c89b5c
ChethiyaNuwan <gunasekaramgms.20@uom.lk>
2023-12-04 11:03:15 +0530

    created new user page

--- frontend/src/pages/NewUser.js
+++ frontend/src/pages/NewUser.js
@@ -1,53 +1,99 @@
 import React, { useState } from "react";
-import { Button, TextField } from "@mui/material";
+import { Button, TextField, Stack } from "@mui/material";
 import Header from "../components/header";
 
 export default function NewUser() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [address, setAddress] = useState("");
-    const [phones, setPhones] = useState([]);
-    
-    const handleAddUser = async () => {
-        const token = localStorage.getItem("token");
-        const response = await fetch("http://localhost:5000/users", {
-        method: "POST",
-        body: JSON.stringify({ name, email, address }),
-        });
-        if (response.ok) {
-        window.location.href = "/users";
-        }
-    };
-    
-    return (
-        <>
-        <Header title="Add New User" />
-        <form id="newUserForm">
-            <TextField
-            id="name"
-            label="Name"
-            variant="outlined"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            />
-            <TextField
-            id="email"
-            label="Email"
-            variant="outlined"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-            id="address"
-            label="Address"
-            variant="outlined"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            />
-            <Button variant="contained" onClick={handleAddUser}>
-            Add User
-            </Button>
-        </form>
-        </>
-    );
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [phones, setPhones] = useState([{ number: "", description: "" }]);
+
+  const handleAddPhone = () => {
+    setPhones((prevPhones) => [...prevPhones, { number: "", description: "" }]);
+  };
+
+  const handleRemovePhone = (index) => {
+    const newPhones = [...phones];
+    newPhones.splice(index, 1);
+    setPhones(newPhones);
+  };
+
+  const handlePhoneChange = (event, index) => {
+    const newPhones = [...phones];
+    const { name, value } = event.target;
+    if (name === "number") {
+      newPhones[index].number = value;
+    } else {
+      newPhones[index].description = value;
     }
+    setPhones(newPhones);
+  };
+
+  const handleAddUser = async () => {};
+
+  return (
+    <>
+      <Header title="Add New User" />
+      <form id="newUserForm">
+        <TextField
+          id="name"
+          label="Name"
+          variant="outlined"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <TextField
+          id="email"
+          label="Email"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <TextField
+          id="address"
+          label="Address"
+          variant="outlined"
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+
+        <Stack spacing={2}>
+          {phones && phones.map((phone, index) => (
+            <Stack key={index} direction="row" spacing={2}>
+              <TextField
+                id={index}
+                label="Phone Description"
+                variant="outlined"
+                value={phone.description}
+                onChange={(e) => handlePhoneChange(e, index)}
+                name="description"
+                />
+                <TextField
+                id={index}
+                label="Phone Number"
+                variant="outlined"
+                value={phone.number}
+                onChange={(e) => handlePhoneChange(e, index)}
+                name="number"
+                />
+                <Button
+                variant="contained"
+                id="removePhoneBtn"
+                onClick={() => handleRemovePhone(index)}
+                >
+                Remove
+                </Button>
+            </Stack>
+
+            ))}
+        </Stack>
+        <Button variant="contained" onClick={handleAddPhone}>
+          Add Phone
+        </Button>
+        <Button variant="contained" onClick={handleAddUser}>
+          Add User
+        </Button>
+        </form>
+    </>
+    );
+}
