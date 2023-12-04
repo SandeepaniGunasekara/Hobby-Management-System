4465659da3cceadf533c34b52400cc7922943138
ChethiyaNuwan <gunasekaramgms.20@uom.lk>
2023-12-04 09:42:11 +0530

    configured router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
+  const routes = createBrowserRouter([
+    {path: '/login', element: <Login/>},
+  ]);
+
   return (
-    <></>
+    <RouterProvider router={routes}/>
   );
 }
 
