cf86e9ee5b5d031faf5f2f555eed8e31597f229e
ChethiyaNuwan <gunasekaramgms.20@uom.lk>
2023-12-04 09:49:45 +0530

    created login page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,40 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export default function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-    </div>
-  );
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+    };
+  
+    return (
+      <div className="login-page">
+        <h1>Login</h1>
+        <p id='message'>{message}</p>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            id="email"
+            label="Email"
+            variant="outlined"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <TextField
+            id="password"
+            label="Password"
+            variant="outlined"
+            type="password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <Button type="submit" variant="contained" color="primary">
+            Login
+          </Button>
+        </form>
+      </div>
+    );
 }
